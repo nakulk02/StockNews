2d64adc320605c9791fbce6a9404d10d3c6e4dd9
Aniket Sood <nyp.aniket@gmail.com>
2025-03-10 22:02:07 +0530

    basic frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import Stock from './pages/Stock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/stock' element={<Stock />} />
+        </Routes>
+      </div>
     </div>
   );
 }
